2ab91a029b7578a0424651a8bf67c372e309867b
İzni Burak Demirtaş <izniburak@outlook.com>
2015-06-30 11:41:55 +0300

    Update example.php

--- example.php
+++ example.php
@@ -7,113 +7,63 @@
 *
 * @ Web: http://burakdemirtas.org
 *
-* @ Docs: http://burakdemirtas.org/59/pdox-useful-pdo-class-php/
+* @ Docs: http://burakdemirtas.org/pdox-kullanisli-pdo-sinifi-php/
 *
 * @ Licence: The MIT License (MIT) - Copyright (c) 2014 - http://opensource.org/licenses/MIT
 *
 */
 
-
 	header('content-type: text/html; charset=utf-8');
 
-	
 	// include class
 	include('pdox.class.php');
 
+	$config = [
+		# Host name or IP Address (not required)
+		# default value: localhost
+		'host'      => 'localhost',
+		
+		# Database Driver Type (not required)
+		# default value: mysql
+		# values: mysql, pgsql, sqlite, oracle
+		'driver'    => 'mysql',
+		 
+		# Database Name (required)
+		'database'  => 'test',
+		
+		# Database User Name (required)
+		'username'  => 'root',
+		
+		# Database User Password (required)
+		'password'  => '',
+		
+		# Database Charset (not required)
+		# default value: utf8
+		'charset'   => 'utf8',
+		
+		# Database Charset Collation (not required)
+		# default value: utf8_general_ci
+		'collation' => 'utf8_general_ci',
+		
+		# Database Name Prefix (not required)
+		# default value:
+		'prefix'     => ''
+	];
 	
-	// config values
-	$config = array(
-
-		/*
-		** Database User Name (required)
-		*/
-		
-		'user'		=> 'root',
-		
-		/*
-		** Database User Password (required)
-		*/
-		
-		'pass'		=> '',
-		
-		/* 
-		** Database Name (required)
-		*/
-		
-		'dbname'	=> 'test',
-		
-		/*
-		** Host name or IP Address (not required)
-		** default value: localhost
-		*/
-		
-		'host'		=> 'localhost',
-		
-		/* Database Driver Type (not required)
-		** default value: mysql
-		** values: mysql, pgsql, sqlite, oracle
-		*/
-		
-		'type'		=> 'mysql',
-		
-		/* Database Charset (not required)
-		** default value: utf8
-		*/
-		
-		'charset'	=> 'utf8',
-		
-		/* Database Name Prefix (not required)
-		** default value: 
-		*/
-		
-		'prefix'	=> ''
-		
-	);
-
 	
 	// start PDOx
-	$db = new PDOx($config);
+	$db = new \buki\PDOx($config);
 	
 	
 	// Example
+	$records = $db->select('id, name, surname, age')
+				->from('users')
+				->where('age', '>', 18)
+				->orderBy('id', 'desc')
+				->limit(20)
+				->getAll();
 	
-	$records = $db->select('id, name, surname, age')->from('users')->where('age', '>', 18)->orderBy('id', 'desc')->limit(20)->getAll();
-	
-	foreach($records as $record) {
-	
+	foreach($records as $record)
+	{
 		echo $record->id . ' - ' . $record->name . ' ' . $record->surname . ' / ' . $record->age . '<br />';
-	
 	}
-	
-	
-	/*
-	**
-	** FUNCTIONS LIST
-	**
-	**** select
-	**** from
-	**** where
-	**** orWhere
-	**** whereIn
-	**** whereNotIn
-	**** join
-	**** leftJoin
-	**** rightJoin
-	**** like
-	**** orLike
-	**** orderBy
-	**** groupBy
-	**** having
-	**** limit
-	**** count
-	**** insertId
-	**** error
-	**** get
-	**** getAll
-	**** insert
-	**** update
-	**** delete
-	**** query
-	**** escape
-	**
-	*/
