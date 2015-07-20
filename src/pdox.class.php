5459834b6938e1bb9ba50d10dd9d34c6b212e70f
İzni Burak Demirtaş <izniburak@outlook.com>
2015-07-20 19:26:13 +0300

    changed method from() -> table() and innerJoin method added.

--- src/pdox.class.php
+++ src/pdox.class.php
@@ -81,7 +81,7 @@ class PDOx
 		return $this;
 	}
 
-	public function from($from)
+	public function table($from)
 	{
 		if(is_array($from))
 		{
@@ -97,33 +97,57 @@ class PDOx
 		return $this;
 	}
 	
-	public function join($table, $field1, $op = null, $field2 = null, $join = 'INNER')
+	public function join($table, $field1, $op = null, $field2 = null, $join = '')
 	{
-		$where = $field1;
-		$table = $this->prefix . $table;
-		
-		if(!is_null($op)) 
-			$where = (!in_array($op, $this->op) ? $this->prefix . $field1 . ' = ' . $this->prefix . $op : $this->prefix . $field1 . ' ' . $op . ' ' . $this->prefix . $field2);
-	
-		if (is_null($this->join))
-			$this->join = " " . $join . " JOIN" . " " . $table . " ON " . $where;
-			
+		if(is_array($table))
+		{
+			$q = '';
+			if(count($table) > 3)
+				$q .= strtoupper($table[0]) . ' JOIN ' . $table[1] . ' ON ' . $table[2] . ' = ' . $table[3];
+			else
+				$q .= strtoupper($table[0]) . ' JOIN ' . $table[1] . ' ON ' . $table[2];
+
+			if (is_null($this->join))
+				$this->join = " " . $q;
+
+			else
+				$this->join = $this->join . " " . $q;
+		}
 		else
-			$this->join = $this->join . " " . $join . " JOIN" . " " . $table . " ON " . $where;
+		{
+			$where = $field1;
+			$table = $this->prefix . $table;
+			
+			if(!is_null($op)) 
+				$where = (!in_array($op, $this->op) ? $this->prefix . $field1 . ' = ' . $this->prefix . $op : $this->prefix . $field1 . ' ' . $op . ' ' . $this->prefix . $field2);
 		
+			if (is_null($this->join))
+				$this->join = " " . $join . "JOIN" . " " . $table . " ON " . $where;
+				
+			else
+				$this->join = $this->join . " " . $join . "JOIN" . " " . $table . " ON " . $where;
+		}
+		
+		return $this;
+	}
+	
+	public function innerJoin($table, $field1, $op = '', $field2 = '')
+	{
+		$this->join($table, $field1, $op, $field2, 'INNER ');
+
 		return $this;
 	}
 	
 	public function leftJoin($table, $field1, $op = '', $field2 = '')
 	{
-		$this->join($table, $field1, $op, $field2, 'LEFT');
+		$this->join($table, $field1, $op, $field2, 'LEFT ');
 
 		return $this;
 	}
 	
 	public function rightJoin($table, $field1, $op = '', $field2 = '')
 	{
-		$this->join($table, $field1, $op, $field2, 'RIGHT');
+		$this->join($table, $field1, $op, $field2, 'RIGHT ');
 		
 		return $this;
 	}
