17f061bcf78b56e9c94ab456c518d9ba9eea2c4f
İzni Burak Demirtaş <izniburak@outlook.com>
2015-12-20 04:19:38 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,8 +1,7 @@
 <?php 
 /*
 *
-* @ Package: PDOx - Useful ORM & PDO Class
-*
+* @ Package: PDOx - Useful Query Builder & PDO Class
 * @ Author: izni burak demirtas / @izniburak <info@burakdemirtas.org>
 * @ Web: http://burakdemirtas.org
 * @ URL: https://github.com/izniburak/PDOx
@@ -12,52 +11,27 @@
 
 require 'vendor/autoload.php';
 
+// database config
 $config = [
-	# Host name or IP Address (not required)
-	# default value: localhost
 	'host'      => 'localhost',
-	
-	# Database Driver Type (not required)
-	# default value: mysql
-	# values: mysql, pgsql, sqlite, oracle
 	'driver'    => 'mysql',
-	 
-	# Database Name (required)
 	'database'  => 'test',
-	
-	# Database User Name (required)
 	'username'  => 'root',
-	
-	# Database User Password (required)
 	'password'  => '',
-	
-	# Database Charset (not required)
-	# default value: utf8
 	'charset'   => 'utf8',
-	
-	# Database Charset Collation (not required)
-	# default value: utf8_general_ci
 	'collation' => 'utf8_general_ci',
-	
-	# Database Name Prefix (not required)
-	# default value:
-	'prefix'     => ''
+	'prefix'    => ''
 ];
 
-
 // start PDOx
-$db = new \buki\PDOx($config);
+$db = new \Buki\Pdox($config);
 
 
-// Example
-$records = $db->table('users')
-		->select('id, name, surname, age')
+// Select Records
+$records = $db->table('pages')
 		->where('age', '>', 18)
 		->orderBy('id', 'desc')
-		->limit(20)
+		->limit(10)
 		->getAll();
-
-foreach($records as $record)
-{
-	echo $record->id . ' - ' . $record->name . ' ' . $record->surname . ' / ' . $record->age . '<br />';
-}
+		
+var_dump($records);
